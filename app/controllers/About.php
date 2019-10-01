8838f2f2bcd8e246e43fd7b98d1b0680d1952457
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 19:56:28 +0700

    edit header

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,13 +5,15 @@ class About extends Controller {
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
-        $this->view ('templates/header');
+        $data['judul'] = 'About Me';
+        $this->view ('templates/header', $data);
         $this->view('about/index', $data);
         $this->view ('templates/footer');
     }
 
     public function page(){
-        $this->view ('templates/header');
+        $data['judul'] = 'Pages';
+        $this->view ('templates/header', $data);
         $this->view('about/page');
         $this->view ('templates/footer');
     }
