8b6ddcc1e5e71933b82b25f20fdefac3d1ac5ecb
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 19:27:16 +0700

    edit method index

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,8 +1,11 @@
 <?php
 
 class About extends Controller {
-    public function index($nama = 'Ginanjar', $pekerjaan ='Gamer'){
-        $this->view('about/index');
+    public function index($nama = 'Ginanjar', $pekerjaan ='Gamer', $umur = 22){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('about/index', $data);
     }
 
     public function page(){
