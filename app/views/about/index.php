92af88f00d8c94e39739c1ae6dd46f3b04ff3721
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 19:32:40 +0700

    memanggil view melalui data di file about.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1>About Me</h1>
+    <p>Halo! Nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, dan saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
 </body>
 </html>
