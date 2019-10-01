c78a633bb89d69450afe22fcab6a51e692772494
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 17:59:00 +0700

    menambah function parseURL

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,15 @@
 
 class App {
     public function __construct(){
-        echo "ok";
+        $url = $this->paraseURL();
+        var_dump($url);
+    }
+
+    public function paraseURL(){
+        if ( isset($_GET['url']) ){
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 
 }
