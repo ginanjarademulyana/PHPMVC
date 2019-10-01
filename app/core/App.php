d990f1a3cf5ca3f15563f891d94e575b465c4943
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 18:42:39 +0700

    mengedit function __construct

--- app/core/App.php
+++ app/core/App.php
@@ -5,9 +5,28 @@ class App {
     protected $method = 'index';
     protected $params = [];
 
-    public function __construct(){
-        $url = $this->paraseURL();
-        var_dump($url);
+    public function __construct() {
+        $url = $this->parseURL();
+        
+        if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        if( isset($url[1]) ) {
+            if( method_exists($this->controller , $url[1]) ){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        if( !empty($url) ) {
+            var_dump($url);
+        }
+
     }
 
     public function parseURL(){
