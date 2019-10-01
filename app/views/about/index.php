76a1f56688ba9ff54ff7bd3586e7d2d97338d109
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 19:44:35 +0700

    hapus header dan footer

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,10 +1,2 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Halaman About</title>
-</head>
-<body>
     <h1>About Me</h1>
     <p>Halo! Nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, dan saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
-</body>
-</html>
--- app/views/about/page.php
+++ app/views/about/page.php
@@ -1,9 +1,2 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Halaman Page</title>
-</head>
-<body>
+
     <h1>My Pages</h1>
-</body>
-</html>
