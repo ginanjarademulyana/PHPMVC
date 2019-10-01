5734abcfd5e5405c57a5facb1cd854ef5b6f2cc4
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-10-01 19:14:50 +0700

    edit method view

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,7 +2,7 @@
 
 class Controller {
     public function view($view, $data = []){
-        require_once '../app/view' . $view . '.php';
+        require_once '../app/views/' . $view . '.php';
     }
     
 }
